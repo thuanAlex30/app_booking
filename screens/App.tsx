5807e3d85388d04a21b413fcd9b930bfa75d4d06
lamdanhmaikhongngu <sentai28deka@gmail.com>
2025-02-23 23:36:55 +0700

    cap nhat app

--- screens/App.tsx
+++ screens/App.tsx
@@ -4,31 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import RoomDetailScreen from '../screens/RoomDetailScreen';
 
-// Định nghĩa kiểu dữ liệu cho Room
 type Room = {
     id: string;
     name: string;
     capacity: number;
+    price: number;
 };
 
-// Định nghĩa danh sách route của ứng dụng
 type RootStackParamList = {
     Home: undefined;
     RoomDetail: { room: Room };
 };
 
-// Tạo Stack Navigator với kiểu dữ liệu
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Trang chủ' }} />
-        <Stack.Screen name="RoomDetail" component={RoomDetailScreen} options={{ title: 'Chi tiết phòng' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="RoomDetail" component={RoomDetailScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
-export default AppNavigator;
+export default App;
